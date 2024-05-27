b2aa9c1257e44e085ce29477c7b9d589a5cdf0ac
Steeven03 <160257856+Steeven03@users.noreply.github.com>
2024-05-27 11:50:55 -0500

    Estoy trabajando con una nueva tecnologia

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		translate: {
+		  '1.25': '-5px', // Define el valor de -5px para translateY
+		},
+		colors: {
+		  'custom-green': 'rgba(163, 231, 132, 1)', // Define el color personalizado
+		},
+
+		margin: {
+			'45': '45px',  // Clase personalizada para el margen superior
+			'25': '25px',  // Clase personalizada para el margen inferior
+		  },
+	  },
+	},
+	variants: {
+	  extend: {
+		translate: ['focus'], // Habilita el uso de translate en el estado focus
+	  },
 	},
 	plugins: [],
-}
+  }
+  
