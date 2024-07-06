38e78a56f29774bc3171a3862025c20223b8ffc3
Jessica-Timaran <jessicatimaran1@gmail.com>
2024-07-06 12:37:33 -0500

    navbar 2 y boton de sidebar mas avance de responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // o 'media'
 	theme: {
-		extend: {colors: {
-		
-			'Color_carta': '#FBFCFF',
-			'custom-green': 'rgba(163, 231, 132, 1)',
-			
-	
-			},},
+	  extend: {
+		colors: {
+		  'Color_carta': '#FBFCFF',
+		  'custom-green': 'rgba(163, 231, 132, 1)',
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
