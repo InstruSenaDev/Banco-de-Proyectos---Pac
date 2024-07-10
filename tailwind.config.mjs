5cf3bae0e01051cc587a6463e619fe24112f8f44
Steeven03 <angelicacb6392@gmail.com>
2024-07-10 08:45:27 -0500

    vistas del instructor

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // o 'media'
 	theme: {
 	  extend: {
-		translate: {
-		  '1.25': '-5px', // Define el valor de -5px para translateY
-		},
 		colors: {
-		  'custom-green': 'rgba(163, 231, 132, 1)', // Define el color personalizado
+		  'Color_carta': '#FBFCFF',
+		  'custom-green': 'rgba(163, 231, 132, 1)',
 		},
-
-	  },
-	},
-	variants: {
-	  extend: {
-		translate: ['focus'], // Habilita el uso de translate en el estado focus
 	  },
 	},
 	plugins: [],
   }
-  
