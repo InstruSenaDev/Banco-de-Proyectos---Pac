dcb78a1d78b5da158c063fd84adaccd076eedb9b
Steeven03 <angelicacb6392@gmail.com>
2024-07-17 11:30:16 -0500

    conectando la base de datos

---
+++ Backend/routes/dataroutes.js
@@ -0,0 +1,42 @@
+// Backend/routes/dataroutes.js
+import express from 'express';
+import { pool } from '../config/db.js';
+import { getAllPersonas, getAllUsuario } from '../controllers/datacontroler.js';
+
+const router = express.Router();
+
+// Ruta para obtener todas las personas
+router.get('/personas', async (req, res) => {
+    try {
+        const personas = await getAllPersonas();
+        res.json(personas);
+    } catch (error) {
+        console.error('Error al obtener personas:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Ruta para obtener todos los usuarios
+router.get('/usuarios', async (req, res) => {
+    try {
+        const usuarios = await getAllUsuario();
+        res.json(usuarios);
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.post('/register', async (req, res) => {
+    try {
+        const { nombre, tipodocumento, numerodocumento, nombreempresa, telefono, correo, contraseña, idrol } = req.body;
+        const newPerson = await registerPerson({ nombre, tipodocumento, numerodocumento, nombreempresa, telefono, correo, contraseña, idrol });
+        res.status(201).json(newPerson);
+    } catch (error) {
+        console.error('Error al registrar persona:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+export default router;
