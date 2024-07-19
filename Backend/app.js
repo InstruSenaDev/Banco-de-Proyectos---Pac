c54edbe8e7dff575faf918e0a4f31bc4bc5be454
YuranyRivera <yuranidelacruz2006.com>
2024-07-19 18:04:37 -0500

    responsive superadmin

--- Backend/app.js
+++ Backend/app.js
@@ -22,5 +22,5 @@ app.use((err, req, res, next) => {
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log("Server is running on port http://localhost:${PORT}"); 
-});
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
