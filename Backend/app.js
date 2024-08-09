6012583e1b3d7e4aaf0efb6aa6e907206a1d40bc
Karen <Kadayana51@gmail.com>
2024-08-09 10:47:24 -0500

    Conexion

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import datarouter from './routes/datarouter.js'; // Asegúrate de la ruta correcta
+import router from './routes/dataroutes.js'; // Asegúrate de que la ruta sea la correcta
 
 const app = express();
 app.use(express.json());
 
-app.use('/api', datarouter); // Montar el enrutador en la ruta /api
+app.use('/api', router); // Asegúrate de usar '/api' como prefijo si así lo has definido
 
-const port = 4000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
