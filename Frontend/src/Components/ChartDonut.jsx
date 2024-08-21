df7e83e5b3e1c1e02266292c2012205c72807726
Steeven-Gomez <steevengomez2709@gmail.com>
2024-08-21 18:08:59 -0500

    cambios

--- Frontend/src/Components/ChartDonut.jsx
+++ Frontend/src/Components/ChartDonut.jsx
@@ -1,12 +1,12 @@
 import { DonutChart } from '@tremor/react';
 
+
 const datahero = [
-  { name: 'Noche Holding AG', value: 9800 },
-  { name: 'Rain Drop AG', value: 4567 },
-  { name: 'Push Rail AG', value: 3908 },
-  { name: 'Flow Steal AG', value: 2400 },
-  { name: 'Tiny Loop Inc.', value: 2174 },
-  { name: 'Anton Resorts Holding', value: 1398 },
+  { name: 'Proyectos recibidos', value: 980 },
+  { name: 'Proyectos rechazados', value: 4567 },
+  { name: 'Proyectos aceptados', value: 3908 },
+  { name: 'Proyectos devueltos', value: 2400 },
+  { name: 'Proyectos por revisar', value: 2174 },
 ];
 
 const dataFormatter = (number) => `$ ${Intl.NumberFormat('us').format(number)}`;
@@ -25,23 +25,12 @@ export const ChartDonut = () => {
             variant="donut"
             valueFormatter={dataFormatter}
             onValueChange={(value) => console.log(value)}
+            colors={["yellow", "red" , "indigo" , "blue" , "green"]}
           />
         </div>
       </div>
       {/* Pie Chart */}
-      <div className="space-y-3">
-        <span className="text-center block font-mono text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-          Pie Variant
-        </span>
-        <div className="flex justify-center">
-          <DonutChart
-            data={datahero}
-            variant="pie"
-            valueFormatter={dataFormatter}
-            onValueChange={(value) => console.log(value)}
-          />
-        </div>
-      </div>
+
     </div>
   );
 };
