cdadc0fc75d80e9da4f6f42cfe17457afba1ed45
Michel <160735145+Kadayana@users.noreply.github.com>
2024-08-23 11:04:38 -0500

    Areas

---
+++ Frontend/src/Components/Prueba.jsx
@@ -0,0 +1,9 @@
+import { Switch } from '@tremor/react';
+
+export function Prueba() {
+  return (
+    <div className="mx-auto flex items-center justify-center">
+      <Switch color={"red"} />
+    </div>
+  );
+}
