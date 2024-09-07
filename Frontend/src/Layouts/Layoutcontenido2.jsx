9b39429addfc625e622dc1f9e5619a27e303100e
Steeven03 <steevengomez2709@gmail.com>
2024-09-07 10:43:19 -0500

    Estilo

--- Frontend/src/Layouts/Layoutcontenido2.jsx
+++ Frontend/src/Layouts/Layoutcontenido2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layoutcontenido2 = ({ text1, children }) => {
   return (
-    <main className="flex flex-col justify-center items-center h-auto min-w-full lg:pb-10 ">
+    <main className="flex flex-col justify-center items-center h-auto  min-w-full lg:pb-10 2xl:h-screen ">
       <div className="w-full flex justify-center lg:mt-8">
         <h1 className="text-3xl font-bold font-josefin-slab text-center m-8">{text1}</h1>
       </div>
