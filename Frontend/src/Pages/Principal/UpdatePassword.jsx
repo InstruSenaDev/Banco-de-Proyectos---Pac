82930c277e4ecfc1a77c273822b3e9580dfe3409
Jessica Timaran <jessicatimaran1@gmail.com>
2024-10-01 11:43:04 -0500

    Olvidar contraseña y token

--- Frontend/src/Pages/Principal/UpdatePassword.jsx
+++ Frontend/src/Pages/Principal/UpdatePassword.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Input from '../../Components/Input.jsx';
-
 import BotonSegundo from '../../Components/BotonSegundo.jsx';
 import Img2 from '../../../public/Img/usuario.png';
 
@@ -82,16 +81,16 @@ const UpdatePassword = () => {
 
     return (
         <div className=" flex justify-center  ">
-        <div className=" w-[100em]  flex column items-center ">
-          <div className="bg-white  rounded-lg  w-[40%] flex flex-col items-center   ">
-          <div className="logo-sena flex m-auto items-center justify-center w-56 h-20 sm:w-72 sm:h-24 lg:w-80 lg:h-28 2xl:w-96 2xl:h-32 bg-[#A3E784] rounded-bl-[40px] rounded-br-[40px]">
-              <img
-                className="w-12 h-12 sm:w-16 sm:h-16 lg:w-20 lg:h-20"
-                src="/public/img/logo.png"
-                alt="Logo Sena"
-              />
-            </div>
-            <h2 className="text-[33px] font-bold text-gray-800 text-center mt-[10%]  ">
+            <div className=" w-[100em]  flex column items-center ">
+                <div className="bg-white  rounded-lg  w-[40%] flex flex-col items-center   ">
+                    <div className="logo-sena flex m-auto items-center justify-center w-56 h-20 sm:w-72 sm:h-24 lg:w-80 lg:h-28 2xl:w-96 2xl:h-32 bg-[#A3E784] rounded-bl-[40px] rounded-br-[40px]">
+                        <img
+                            className="w-12 h-12 sm:w-16 sm:h-16 lg:w-20 lg:h-20"
+                            src="/public/img/logo.png"
+                            alt="Logo Sena"
+                        />
+                    </div>
+                    <h2 className="text-[33px] font-bold text-gray-800 text-center mt-[10%]">
                         Actualizar Contraseña
                     </h2>
                     <form onSubmit={handleSubmit} className="mt-6" id="update-password-form">
